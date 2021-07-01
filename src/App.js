a027168ae5089215110561353d2f251129655d91
FlyaginAs <rus.reg.36@gmail.com>
2021-07-01 17:24:28 +0300

    ending algorithms1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
 
+/************************************************************* */
+function myReplace(str, before, after) {
+  let isCapital = (word) => {
+    let regExp = new RegExp(/^[A-Z]/);
+    return regExp.test(word);
+  };
+  let toCapital = (word) => {
+    return word.replace(/^\w/, (char) => {
+      return char.toUpperCase();
+    });
+  };
+
+  let newStr = str.replace(before, () => {
+    //определить регистр буквы в заменяемом слове
+    if (isCapital(before)) {
+      return toCapital(after);
+    } else {
+      return after.toLowerCase();
+    }
+  });
+  return newStr;
+}
+
+console.log(
+  myReplace('I think we should look up there', 'up', 'Down')
+);
+/**************************************************************/
+
 function App() {
   return (
     <div className="App">
