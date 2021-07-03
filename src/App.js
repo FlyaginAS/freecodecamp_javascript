c904d0440868fcf39d8703b30ccb9a39b7291d18
FlyaginAs <rus.reg.36@gmail.com>
2021-07-03 16:18:55 +0300

    Everything Be True

--- src/App.js
+++ src/App.js
@@ -2,23 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 
 /************************************************************* */
-//Binary Agents
-function binaryAgent(str) {
-  let arr = str.split(' ');
-
-  console.log(arr);
-  //from binary to decimal
-  arr = arr.map((item) => parseInt(item, 2));
-  console.log(arr);
-
-  //from decimal to char uses String.fromCharCode
-  let resultStr = String.fromCharCode(...arr);
-  return resultStr;
+//Everything Be True
+function truthCheck(collection, pre) {
+  return collection.every((item) => item[pre]);
 }
 
 console.log(
-  binaryAgent(
-    '01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111'
+  truthCheck(
+    [
+      { user: 'Tinky-Winky', sex: 'male' },
+      { user: 'Dipsy' },
+      { user: 'Laa-Laa', sex: 'female' },
+      { user: 'Po', sex: 'female' },
+    ],
+    'sex'
   )
 );
 /**************************************************************/
