d34942ff69dd159579e0ebbdddfa1d8b8926be42
FlyaginAs <rus.reg.36@gmail.com>
2021-07-03 16:13:21 +0300

    Binary Agents

--- src/App.js
+++ src/App.js
@@ -2,34 +2,29 @@ import logo from './logo.svg';
 import './App.css';
 
 /************************************************************* */
-//flat without flat
-function steamrollArray(arr) {
-  let result = [].concat(...arr);
-  return result.some(Array.isArray) ? steamrollArray(result) : result;
+//Binary Agents
+function binaryAgent(str) {
+  let arr = str.split(' ');
+
+  console.log(arr);
+  //from binary to decimal
+  arr = arr.map((item) => parseInt(item, 2));
+  console.log(arr);
+
+  //from decimal to char uses String.fromCharCode
+  let resultStr = String.fromCharCode(...arr);
+  return resultStr;
 }
 
-console.log(steamrollArray([1, [2], [3, [[4]]]]));
+console.log(
+  binaryAgent(
+    '01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111'
+  )
+);
 /**************************************************************/
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
