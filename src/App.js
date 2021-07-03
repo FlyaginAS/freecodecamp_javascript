d4899019e5d110ee0faa0ceb2461b426af60b56c
FlyaginAs <rus.reg.36@gmail.com>
2021-07-03 16:52:37 +0300

    Arguments Optional

--- src/App.js
+++ src/App.js
@@ -2,22 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 
 /************************************************************* */
-//Everything Be True
-function truthCheck(collection, pre) {
-  return collection.every((item) => item[pre]);
+//Arguments Optional
+function addTogether(...args) {
+  if (args.length === 2) {
+    return typeof args[0] === 'number' && typeof args[1] === 'number'
+      ? args[0] + args[1]
+      : undefined;
+  } else {
+    if (typeof args[0] !== 'number') {
+      return undefined;
+    }
+    return function (b) {
+      return typeof args[0] === 'number' && typeof b === 'number'
+        ? args[0] + b
+        : undefined;
+    };
+  }
 }
 
-console.log(
-  truthCheck(
-    [
-      { user: 'Tinky-Winky', sex: 'male' },
-      { user: 'Dipsy' },
-      { user: 'Laa-Laa', sex: 'female' },
-      { user: 'Po', sex: 'female' },
-    ],
-    'sex'
-  )
-);
+console.log(addTogether('http://bit.ly/IqT6zt'));
 /**************************************************************/
 
 function App() {
